a9f494e84069f7c7c45117e24176e35edeaaaadb
moyy <moyy@qq.com>
2022-02-21 21:09:28 +0800

    feat: 渲染图 执行器

--- crates/pi_render/src/graph/runner.rs
+++ crates/pi_render/src/graph/runner.rs
@@ -6,15 +6,15 @@ use super::{
 };
 use crate::{
     graph::{context::RenderNodeContext, edge::Edge},
-    rhi::device::RenderDevice,
+    rhi::{device::RenderDevice, RenderQueue},
 };
+use async_graph::async_graph;
+use graph::{NGraph, NGraphBuilder};
 use hash::XHashMap;
 use pi_ecs::prelude::World;
-use smallvec::{smallvec, SmallVec};
-use std::{borrow::Cow, collections::VecDeque};
+use r#async::rt::{AsyncRuntime, AsyncTaskPool, AsyncTaskPoolExt};
+use std::{borrow::Cow, cell::RefCell, collections::VecDeque, sync::Arc};
 use thiserror::Error;
-
-pub(crate) struct RenderGraphRunner;
 
 #[derive(Error, Debug)]
 pub enum RenderGraphRunnerError {
@@ -41,149 +41,61 @@ pub enum RenderGraphRunnerError {
     },
 }
 
-impl RenderGraphRunner {
-    pub fn run(
-        graph: &RenderGraph,
+// 渲染图 执行器
+pub(crate) struct RenderGraphRunner<O, P>
+where
+    O: Default + 'static,
+    P: AsyncTaskPoolExt<O> + AsyncTaskPool<O>,
+{
+    // 异步运行时
+    rt: AsyncRuntime<O, P>,
+    // 渲染图, RefCell 是为了 外部 可以 根据情况 修改
+    render_graph: Arc<RefCell<RenderGraph>>,
+    // 异步图, build 实现
+    agraph: Arc<NGraph>,
+}
+
+impl<O, P> RenderGraphRunner<O, P>
+where
+    O: Default + 'static,
+    P: AsyncTaskPoolExt<O> + AsyncTaskPool<O>,
+{
+    // 根据 渲染图 构建 执行器
+    pub fn new(
+        rt: AsyncRuntime<O, P>,
+        render_graph: Arc<RefCell<RenderGraph>>,
         device: RenderDevice,
         queue: &wgpu::Queue,
-        world: &World,
-    ) -> Result<(), RenderGraphRunnerError> {
-        let commands = device.create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
+        world: World,
+    ) -> std::io::Result<Self> {
+        let builder = NGraphBuilder::new();
 
         let res_mgr = XHashMap::new();
 
+        // TODO 遍历每个 Node
+        let commands = device.create_command_encoder(&wgpu::CommandEncoderDescriptor::default());
         let mut render_context = RenderContext {
             device,
             commands,
             res_mgr,
         };
+        queue.submit(vec![render_context.commands.finish()]);
 
-        Self::run_graph(graph, None, &mut render_context, world, &[])?;
+        // TODO 从 终点 反向遍历每条边
 
-        {
-            queue.submit(vec![render_context.command_encoder.finish()]);
-        }
-        Ok(())
+        let agraph = builder.build()?;
+        let agraph = Arc::new(agraph);
+        Some(Self {
+            rt,
+            agraph,
+            render_graph,
+        })
     }
 
-    fn run_graph(
-        graph: &RenderGraph,
-        graph_name: Option<Cow<'static, str>>,
-        render_context: &mut RenderContext,
-        world: &World,
-        inputs: &[SlotValue],
-    ) -> Result<(), RenderGraphRunnerError> {
-        let mut node_outputs: XHashMap<NodeId, SmallVec<[SlotValue; 4]>> = XHashMap::default();
+    pub async fn run(&mut self, queue: RenderQueue) {
+        let agraph = self.agraph;
+        let _ = async_graph(AsyncRuntime::Multi(self.rt.clone()), agraph).await;
 
-        // Queue up nodes without inputs, which can be run immediately
-        let mut node_queue: VecDeque<&NodeState> = graph
-            .iter_nodes()
-            .filter(|node| node.input_slots.is_empty())
-            .collect();
-
-        // pass inputs into the graph
-        if let Some(input_node) = graph.input_node() {
-            let mut input_values: SmallVec<[SlotValue; 4]> = SmallVec::new();
-            for (i, input_slot) in input_node.input_slots.iter().enumerate() {
-                if let Some(input_value) = inputs.get(i) {
-                    if input_slot.slot_type != input_value.slot_type() {
-                        return Err(RenderGraphRunnerError::MismatchedInputSlotType {
-                            slot_index: i,
-                            actual: input_value.slot_type(),
-                            expected: input_slot.slot_type,
-                            label: input_slot.name.clone().into(),
-                        });
-                    } else {
-                        input_values.push(input_value.clone());
-                    }
-                } else {
-                    return Err(RenderGraphRunnerError::MissingInput {
-                        slot_index: i,
-                        slot_name: input_slot.name.clone(),
-                        graph_name: graph_name.clone(),
-                    });
-                }
-            }
-
-            node_outputs.insert(input_node.id, input_values);
-
-            for (_, node_state) in graph.iter_node_outputs(input_node.id).expect("node exists") {
-                node_queue.push_front(node_state);
-            }
-        }
-
-        'handle_node: while let Some(node_state) = node_queue.pop_back() {
-            // skip nodes that are already processed
-            if node_outputs.contains_key(&node_state.id) {
-                continue;
-            }
-
-            let mut slot_indices_and_inputs: SmallVec<[(usize, SlotValue); 4]> = SmallVec::new();
-            // check if all dependencies have finished running
-            for (edge, input_node) in graph
-                .iter_node_inputs(node_state.id)
-                .expect("node is in graph")
-            {
-                match edge {
-                    Edge::SlotEdge {
-                        output_index,
-                        input_index,
-                        ..
-                    } => {
-                        if let Some(outputs) = node_outputs.get(&input_node.id) {
-                            slot_indices_and_inputs
-                                .push((*input_index, outputs[*output_index].clone()));
-                        } else {
-                            node_queue.push_front(node_state);
-                            continue 'handle_node;
-                        }
-                    }
-                    Edge::NodeEdge { .. } => {
-                        if !node_outputs.contains_key(&input_node.id) {
-                            node_queue.push_front(node_state);
-                            continue 'handle_node;
-                        }
-                    }
-                }
-            }
-
-            // construct final sorted input list
-            slot_indices_and_inputs.sort_by_key(|(index, _)| *index);
-            let inputs: SmallVec<[SlotValue; 4]> = slot_indices_and_inputs
-                .into_iter()
-                .map(|(_, value)| value)
-                .collect();
-
-            assert_eq!(inputs.len(), node_state.input_slots.len());
-
-            let mut outputs: SmallVec<[Option<SlotValue>; 4]> =
-                smallvec![None; node_state.output_slots.len()];
-            {
-                let mut context = RenderNodeContext::new(graph, node_state, &inputs, &mut outputs);
-
-                node_state.node.run(&mut context, render_context, world)?;
-            }
-
-            let mut values: SmallVec<[SlotValue; 4]> = SmallVec::new();
-            for (i, output) in outputs.into_iter().enumerate() {
-                if let Some(value) = output {
-                    values.push(value);
-                } else {
-                    let empty_slot = node_state.output_slots.get_slot(i).unwrap();
-                    return Err(RenderGraphRunnerError::EmptyNodeOutputSlot {
-                        type_name: node_state.type_name,
-                        slot_index: i,
-                        slot_name: empty_slot.name.clone(),
-                    });
-                }
-            }
-            node_outputs.insert(node_state.id, values);
-
-            for (_, node_state) in graph.iter_node_outputs(node_state.id).expect("node exists") {
-                node_queue.push_front(node_state);
-            }
-        }
-
-        Ok(())
+        // TODO 执行 present
     }
 }
